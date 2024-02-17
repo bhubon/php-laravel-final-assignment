f622319ba625e3e30990e4fda8d4aa66e0454ecb
Bhubon Sutradhar Nil <sdbhubon@gmail.com>
2024-02-17 23:43:32 +0600

    init

---
+++ database/migrations/2024_02_17_173406_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('company_name');
+            $table->string('company_address');
+            $table->string('company_phone');
+            $table->string('company_email');
+            $table->string('company_website');
+            $table->string('industry');
+            $table->string('company_size');
+            $table->string('logo');
+            $table->string('cover_photo');
+            $table->string('facebook_links');
+            $table->string('facebook_links');
+            $table->string('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
